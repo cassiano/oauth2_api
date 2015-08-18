52214efe2cf15461b69fa933ffe77052b2499643
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2015-08-18 16:54:06 -0300

    Facilitando a busca por recursos via API

--- lib/api/base.rb
+++ lib/api/base.rb
@@ -4,12 +4,17 @@ class Api::Base
   attribute :access_token, OAuth2::AccessToken
   attribute :links, Array
 
-  def self.load_resource(access_token, uri, params = {})
-    access_token.get(uri, params: params).parsed
+  def self.load_resource(access_token, uri, params, klass, additional_attrs = {})
+    result = access_token.get(uri, params: params).parsed
+
+    case result
+      when Hash   then klass.new(result.merge(additional_attrs))
+      when Array  then result.map { |attrs| klass.new(attrs.merge(additional_attrs)) }
+    end
   end
 
-  def load_resource(link_name, params = {})
-    self.class.load_resource(access_token, link(link_name), params)
+  def load_resource(link_name, params, klass, additional_attrs = {})
+    self.class.load_resource access_token, link(link_name), params, klass, additional_attrs
   end
 
   def link(name)
--- lib/api/user.rb
+++ lib/api/user.rb
@@ -3,20 +3,16 @@ class Api::User < Api::Base
   attribute :email, String
 
   def self.from_token(access_token)
-    new load_resource(access_token, 'api/v2/users/whoami.json').merge(access_token: access_token)
+    load_resource access_token, 'api/v2/users/whoami.json', {}, self, access_token: access_token
   end
 
   def tasks(reload = false)
     @tasks = nil if reload
 
-    @tasks ||= load_resource(:tasks).map do |task_attrs|
-      Api::Task.new task_attrs.merge(user: self)
-    end
+    @tasks ||= load_resource(:tasks, {}, Api::Task, user: self)
   end
 
   def search_tasks(title, options = {})
-    load_resource(:search, options.merge(title: title)).map do |task_attrs|
-      Api::Task.new task_attrs.merge(user: self)
-    end
+    load_resource :search, options.merge(title: title), Api::Task, user: self
   end
 end
