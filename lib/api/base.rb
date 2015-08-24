1c0abe322dfeb91b84f1bc2f9d40e7a9082cb409
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2015-08-24 17:00:30 -0300

    Gerando métodos (links) automaticamente

--- lib/api/base.rb
+++ lib/api/base.rb
@@ -4,20 +4,46 @@ class Api::Base
   attribute :access_token, OAuth2::AccessToken
   attribute :links, Array
 
-  def self.load_resource(access_token, uri, params, klass, additional_attrs = {})
-    result = access_token.get(uri, params: params).parsed
+  def self.create(attrs)
+    links = attrs[:links]
 
-    case result
-      when Hash   then klass.new(result.merge(additional_attrs))
-      when Array  then result.map { |attrs| klass.new(attrs.merge(additional_attrs)) }
+    new(attrs).tap do |object|
+      if links
+        links.each do |link|
+          link_name = link[:rel].to_sym
+
+          object.define_singleton_method link_name do |link_params, klass, attrs|
+            follow_link link_name, link_params, klass, attrs
+          end
+        end
+      end
     end
   end
+
+  def self.load_resource(access_token, uri, params, klass, additional_attrs = {})
+    json = access_token.get(uri, params: params).parsed
+
+    case json
+      when Hash then
+        attrs = HashWithIndifferentAccess.new(json)
+
+        klass.create attrs.merge(additional_attrs)
+      when Array then
+        json.map do |attrs|
+          attrs = HashWithIndifferentAccess.new(attrs)
+
+          klass.create attrs.merge(additional_attrs)
+        end
+    end
+  end
+
+  protected
 
   def follow_link(link_name, params, klass, additional_attrs = {})
     self.class.load_resource access_token, link(link_name), params, klass, additional_attrs
   end
 
   def link(name)
-    links && (link = links.find { |link| link['rel'] == name.to_s }) && link['href']
+    links && (link = links.find { |link| link[:rel] == name.to_s }) && link[:href]
   end
 end
--- lib/api/user.rb
+++ lib/api/user.rb
@@ -5,14 +5,4 @@ class Api::User < Api::Base
   def self.from_token(access_token)
     load_resource access_token, 'api/v2/users/whoami.json', {}, self, access_token: access_token
   end
-
-  def tasks(reload = false)
-    @tasks = nil if reload
-
-    @tasks ||= follow_link(:tasks, {}, Api::Task, user: self)
-  end
-
-  def search_tasks(title, options = {})
-    follow_link :search_tasks, options.merge(title: title), Api::Task, user: self
-  end
 end
